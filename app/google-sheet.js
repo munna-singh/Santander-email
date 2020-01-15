46e3ebad811d40e9eccebdac58ff7806ecc2eac1
munna-singh <dreammunna@yahoo.co.in>
2020-01-15 20:07:08 +0530

    Excelsheet columnwise data read

--- app/google-sheet.js
+++ app/google-sheet.js
@@ -1,25 +1,26 @@
 const fetch = require('node-fetch');
 
-module.exports = function(google_sheet, google_api_key, cache_duration) {
+module.exports = function(google_sheet, google_api_key, cache_duration, should_transpose_values=true) {
     let cached_data = "{}";
     let cached_time = -1;
 
-    async function get_send_list() {
+    async function get_data() {
         const raw_data = await get_raw_data();
-        const { send_list, headers } = parse_send_list(raw_data);
+        const { data, headers, rows } = parse_data(raw_data);
         
         return { 
-            //raw_data,
-            send_list,
+            raw_data,
+            data,
             headers,
+            rows,
             google_sheet
         };
     }
 
     async function get_single(template_name, variation_name) {
-        const { send_list } = await get_send_list();
+        const { data } = await get_data();
 
-        return send_list[template_name][variation_name];
+        return data[template_name][variation_name];
     }
 
     async function get_raw_data() {
@@ -45,12 +46,45 @@ module.exports = function(google_sheet, google_api_key, cache_duration) {
         return data.replace(/\\n/g, '');
     }
 
-    function parse_send_list(data) {
+    function transpose(a) {
+        // Calculate the width and height of the Array
+        var w = a.length || 0;
+        var h = a[0] instanceof Array ? a[0].length : 0;
+
+        // In case it is a zero matrix, no transpose routine needed.
+        if (h === 0 || w === 0) { return []; }
+
+        var i, j, t = [];
+
+        // Loop through every item in the outer array (height)
+        for (i = 0; i < h; i++) {
+
+            // Insert a new row (array)
+            t[i] = [];
+
+            // Loop through every item per item in outer array (width)
+            for (j = 0; j < w; j++) {
+
+                // Save transposed data.
+                t[i][j] = a[j][i];
+            }
+        }
+
+        return t;
+    }
+
+    function parse_data(data) {
         const json = JSON.parse(clean_google_data(data));
 
+        if (should_transpose_values) {
+            json.values = transpose(json.values);
+        }
+
         json.values.shift();
+
         var headers = ['ID', ...json.values.shift()];
         var result = {};
+        var rows = [];
 
         var row_index = 1;
         for (var row of json.values) {
@@ -66,15 +100,16 @@ module.exports = function(google_sheet, google_api_key, cache_duration) {
                 result[named_row.template_name] = template;
             }
             template[named_row.variation_name] = named_row;
+            rows.push(named_row);
         }
 
-        return { send_list: result, headers };
+        return { data: result, headers, rows };
     }
 
     return {
         get_raw_data,
-        get_send_list,
+        get_data,
         get_single,
-        parse_send_list,
+        parse_data,
     };
 }
